6c5c34153bbb7d0e2a5dbea24b34aba7e2c3f547
Sayli555 <96167212+Sayli555@users.noreply.github.com>
2022-04-09 22:54:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter,setCounter]=useState(0);
+
+  const handlechange=(value)=>{
+    setCounter(counter+value);
+  }
+
+  const handleDouble=(value)=>{
+    setCounter(counter*value);
+  }
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+        
+          <h1 className={`${counter%2}==0 ? "red" :"green}`} >
+            Counter:{counter}</h1>
+        
+      
+        
+        <button onClick={()=>{
+          handlechange(1)
+        }}>
+          ADD 
+        </button>
+         <button onClick={()=>{
+          handlechange(-1)
+        }}>
+          SUB
+         </button>
+         <button onClick={()=>{
+           handleDouble(2)
+         }}>
+           Double
+         </button>
     </div>
   );
 }
